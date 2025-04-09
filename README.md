ac5a170ede1f09e6dcba6216331070fa704ed368
Jacob <osarukun@live.com>
2025-04-09 18:39:13 -0400

    Initial Python circle + collision

---
+++ README.md
@@ -0,0 +1,24 @@
+# Gravity Simulator
+
+## Todo
+
+1. Gravity (Masses pulling towards each other)
+   (mass of object 1 _ mass of object 2 _ Gravitational Constant) / distance ^ 2
+
+   = g force which can convert to acceleration (it's a measure of acceleration after all)
+
+   Each object will exert gravity ... on each other. Changing its acceleration.
+
+2. Convert to 3d
+
+3. Convert Gravity to consider relativity
+
+Matter curving space-time. Normal force is resisting. Space tells matter how to move, matter tells space how to curve.
+
+Grid to show natural curvature of spacetime.
+
+Could do 2d grid. Might try 3d. Idk if I'm Him.
+
+4. spawn objects in + UI to add things.
+
+5. UI with prerendered/determined shapes. like the solar system
--- main.py
+++ main.py
@@ -1,18 +1,77 @@
 from ursina import *
 
-# create a window
 app = Ursina()
 
-planet = Entity(model="sphere", color=color.orange)
+camera.orthographic = True
+camera.fov = 10
 
-speed = 0
-acceleration = -9.8
+planet = Entity(
+    model="sphere", collider="sphere", color=color.orange, position=(0, 2, 0)
+)
+
+speed = Vec3(0, 0, 0)
+acceleration = Vec3(0, -9.81, 0)
+bounce = 0.8
+
+half_width = camera.fov * window.aspect_ratio / 2
+half_height = camera.fov / 2
+thickness: float = 1.0
+
+barriers = []
+
+barriers.append(
+    Entity(
+        model="cube",
+        collider="box",
+        scale=(half_width * 2, thickness, 0.1),
+        position=(0, -half_height, 0),
+        visible=False,
+    )
+)
+
+barriers.append(
+    Entity(
+        model="cube",
+        collider="box",
+        scale=(half_width * 2, thickness, 0.1),
+        position=(0, half_height, 0),
+        visible=False,
+    )
+)
+
+barriers.append(
+    Entity(
+        model="cube",
+        collider="box",
+        scale=(thickness, half_height * 2, 0.1),
+        position=(-half_width, 0, 0),
+        visible=False,
+    )
+)
+
+barriers.append(
+    Entity(
+        model="cube",
+        collider="box",
+        scale=(thickness, half_height * 2, 0.1),
+        position=(half_width, 0, 0),
+        visible=False,
+    )
+)
 
 
 def update():
     global speed
-    planet.y += speed * time.dt
     speed += acceleration * time.dt
+    planet.position += speed * time.dt
+
+    for barrier in barriers:
+        hit_info = planet.intersects(barrier)
+        if hit_info.hit:
+            planet.position -= speed * time.dt
+            normal = hit_info.normal
+            speed = speed - (2 * speed.dot(normal)) * normal
+            speed *= bounce
 
 
 app.run()
